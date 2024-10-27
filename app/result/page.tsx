ec17c4a543d942d532f6db279623e61e8caaa76c
Ash Johns <mail@ashpjohns.com>
2024-10-27 11:00:48 -0400

    check for url search param before making request

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -4,24 +4,28 @@ export default async function Result({
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const url = (await searchParams).url;
-  const res = await fetch(`${process.env.API_URL}/carbon?url=${url}`, {
-    headers: {
-      Authorization: `Bearer ${process.env.API_TOKEN}`,
-    },
-  });
-  const data = await res.json();
-  console.log(data);
+  if (url) {
+    const res = await fetch(`${process.env.API_URL}/carbon?url=${url}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
+      },
+    });
+    console.log(res);
+    const data = await res.json();
 
-  return (
-    <main>
-      <dl>
-        <dt>
-          CO<sub>2</sub>
-        </dt>
-        <dd>{data.co2.toFixed(3)}</dd>
-        <dt>Bytes Transferred</dt>
-        <dd>{data.variables.bytes}</dd>
-      </dl>
-    </main>
-  );
+    return (
+      <main>
+        <dl>
+          <dt>
+            CO<sub>2</sub>
+          </dt>
+          <dd>{data.co2.toFixed(3)}</dd>
+          <dt>Bytes Transferred</dt>
+          <dd>{data.variables.bytes}</dd>
+        </dl>
+      </main>
+    );
+  } else {
+    return <p>Something went wrong with the url.</p>;
+  }
 }
