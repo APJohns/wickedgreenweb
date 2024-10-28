316b6b0398394d70b28b4af0f3ef3c290c9af8e2
Ash Johns <mail@ashpjohns.com>
2024-10-28 19:24:03 -0400

    theme loading page

--- app/testing/loading/page.tsx
+++ app/testing/loading/page.tsx
@@ -13,16 +13,16 @@ export default function Loading() {
     <main className={styles.main}>
       <div className={styles.loader}>
         <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="40" cy="40" r="12" fill="black" />
+          <circle cx="40" cy="40" r="12" fill="black" className={styles.svgFill} />
           <g className={styles.outerOrbit}>
-            <circle cx="40" cy="40" r="36" stroke="black" />
+            <circle cx="40" cy="40" r="36" stroke="black" className={styles.svgStroke} />
             <circle cx="76" cy="40" r="4" fill="#5BC871" />
             <circle cx="4" cy="40" r="4" fill="#5BC871" />
             <circle cx="40" cy="76" r="4" fill="#5BC871" />
             <circle cx="40" cy="4" r="4" fill="#5BC871" />
           </g>
           <g className={styles.innerOrbit}>
-            <circle cx="40" cy="40" r="25" stroke="black" />
+            <circle cx="40" cy="40" r="25" stroke="black" className={styles.svgStroke} />
             <circle cx="40" cy="16" r="4" fill="#5BC871" />
             <circle cx="40" cy="64" r="4" fill="#5BC871" />
           </g>
