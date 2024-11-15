57387c466c8e70f5856e79e9c607cd060b25721b
Ash Johns <mail@ashpjohns.com>
2024-11-15 17:26:32 -0500

    add some styles to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { FormMessage, Message } from '@/components/formMessage';
 import { createClient } from '@/utils/supabase/server';
 import { slugify } from '@/utils/utils';
+import styles from './dashboard.module.css';
 
 export default async function DashboardPage(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
@@ -14,16 +15,32 @@ export default async function DashboardPage(props: { searchParams: Promise<Messa
 
   return (
     <main>
-      <h1>Dashboard</h1>
-      <h2>Projects</h2>
-      <ul>
+      <div className="page-header">
+        <div className="page-header-location">
+          <h1>Projects</h1>
+        </div>
+        <Link href="/dashboard/projects/new" className="icon-action page-header-action">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+          New project
+        </Link>
+      </div>
+      <ul className={styles.projectList}>
         {data?.map((project) => (
           <li key={project.id}>
-            <Link href={`/dashboard/projects/${slugify(project.id)}`}>{project.name}</Link>
+            <Link href={`/dashboard/projects/${slugify(project.id)}`} className={styles.project}>
+              {project.name}
+            </Link>
           </li>
         ))}
       </ul>
-      <Link href="/dashboard/projects/new">New project</Link>
       <FormMessage message={searchParams} />
     </main>
   );
