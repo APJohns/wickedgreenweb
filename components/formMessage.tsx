3ee934bfc92c35d1d772b927f4f97e277f2fe04a
Ash Johns <mail@ashpjohns.com>
2024-11-17 21:24:00 -0500

    turn formMEssage into a toast

--- components/formMessage.tsx
+++ components/formMessage.tsx
@@ -1,11 +1,13 @@
+import styles from './formMessage.module.css';
+
 export type Message = { success: string } | { error: string } | { message: string };
 
 export function FormMessage({ message }: { message: Message }) {
   return (
-    <div>
-      {'success' in message && <p>{message.success}</p>}
-      {'error' in message && <p>{message.error}</p>}
-      {'message' in message && <p>{message.message}</p>}
+    <div className={styles.toast} role="alert">
+      {'success' in message && <p className={styles.success}>{message.success}</p>}
+      {'error' in message && <p className={styles.error}>{message.error}</p>}
+      {'message' in message && <p className={styles.message}>{message.message}</p>}
     </div>
   );
 }
