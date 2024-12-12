430de79454d16a8efe7bac769b50e72dd1f067fb
Ash Johns <mail@ashpjohns.com>
2024-12-11 19:43:22 -0500

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Figtree, Space_Mono } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import Link from 'next/link';
 import { createClient } from '@/utils/supabase/server';
@@ -72,6 +73,7 @@ export default async function RootLayout({
           </nav>
         </header>
         {children}
+        <Analytics />
       </body>
     </html>
   );
